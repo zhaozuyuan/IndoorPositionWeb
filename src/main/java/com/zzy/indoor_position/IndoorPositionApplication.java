c6a0c921090c44d231181a7258383f74655704b9
zhaozuyuan <191868314@qq.com>
2021-02-26 12:57:33 +0800

    创建项目

---
+++ src/main/java/com/zzy/indoor_position/IndoorPositionApplication.java
@@ -0,0 +1,13 @@
+package com.zzy.indoor_position;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndoorPositionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndoorPositionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zzy/indoor_position/IndoorPositionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzy.indoor_position;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndoorPositionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
